4069b69d89c6e4972ef7d244abcdd2c85f2836f1
JeaRRo <jean.ph.navarro@gmail.com>
2016-04-25 00:29:41 +0200

    Mise en place ds salles du musée : tri des MH

--- ini.py
+++ ini.py
@@ -1,11 +1,12 @@
-dep = {'01' : {
-                "01": "Ain",
-                "text":"de l'Ain",
-                "url_d" : "/wiki/Liste_des_monuments_historiques_de_l'Ain",
-                "url_d_2" : "",
-                "zone_osm" : "3600007387",
-                "zone_osm_alt" :"",
-                    },
+dep = {
+#            '01' : {
+#                 "01": "Ain",
+#                 "text":"de l'Ain",
+#                 "url_d" : "/wiki/Liste_des_monuments_historiques_de_l'Ain",
+#                 "url_d_2" : "",
+#                 "zone_osm" : "3600007387",
+#                 "zone_osm_alt" :"",
+#                     },
             # '42' : {
             #     "42":"Loire",
             #     "text" : "de la Loire",
@@ -14,20 +15,20 @@ dep = {'01' : {
             #     "zone_osm" : "3600007420",
             #     "zone_osm_alt" :"",
             #     },
-            '69': {
-                "69":'Rhône',
-                "text": "du Rhône",
-                "url_d" : "/wiki/Liste_des_monuments_historiques_du_Rhône",
-                'url_d_2' : "/wiki/Liste_des_monuments_historiques_de_la_métropole_de_Lyon",
-                "zone_osm" : "3600660056",
-                "zone_osm_alt" :"3604850450",
-                },
-            # '38': {
-            #     "38":'Isère',
-            #     "text": "de l'Isère",
-            #     "url_d" : "/wiki/Liste_des_monuments_historiques_de_l'Isère",
-            #     'url_d_2' : "",
-            #     "zone_osm" : "3600007437",
-            #     "zone_osm_alt" :"",
+            # '69': {
+            #     "69":'Rhône',
+            #     "text": "du Rhône",
+            #     "url_d" : "/wiki/Liste_des_monuments_historiques_du_Rhône",
+            #     'url_d_2' : "/wiki/Liste_des_monuments_historiques_de_la_métropole_de_Lyon",
+            #     "zone_osm" : "3600660056",
+            #     "zone_osm_alt" :"3604850450",
             #     },
+            '38': {
+                "38":'Isère',
+                "text": "de l'Isère",
+                "url_d" : "/wiki/Liste_des_monuments_historiques_de_l'Isère",
+                'url_d_2' : "",
+                "zone_osm" : "3600007437",
+                "zone_osm_alt" :"",
+                },
             }
--- mohist.py
+++ mohist.py
@@ -10,6 +10,7 @@ from collections import OrderedDict
 class Musee:
     '''
         un musée contient des monuments historiques
+        {ref_mh: objet Mohist}
     '''
     def __init__(self,nom=None):
         self.collection = {}
@@ -17,46 +18,80 @@ class Musee:
             self.nom=nom
         else:
             self.nom="Musée_général"
+        #self.salle_merosmwip = Musee("merosmwip")
+
 
     def trier(self):
         '''Renvoie la collection triée'''
         return OrderedDict(sorted(self.collection.items(), key=lambda t: t[0]))
 
-    def calcul_nbMH_merimee(self):
-        '''Renvoie le nombre de MH dans la base Mérimée '''
+    def calcul_nbMH(self,base):
+        '''Renvoie le nombre de MH avec et tag 'base' (mer,osm ou wip) '''
         x=0
         for m,v in self.collection.items():
-            if  v.description[m]['mer']:
-                x+=1
+            # on ne compte pas les doubles dans osm
+            if base == 'osm':
+                if  v.description[m][base] and not "Bis" in m :
+                    x+=1
+            else:
+                if  v.description[m][base]:
+                    x+=1
         return x
 
-    def calcul_nbMH_osm(self):
-        '''Renvoie le nombre de MH dans la base OSM '''
-        x=0
+    def creer_list(self,list_salle):
+        # parcourir les clés des monuments du musée...
+        #noms_salle= ['s_merosmwip','s_merosm','s_merwip','s_osmwip','s_osm','s_wip']
         for m,v in self.collection.items():
-            if  v.description[m]['osm'] and not "Bis" in m :
-                x+=1
-        return x
-
-    def chercher_MerOsm(self):
-        ''' Créer une salle avec les MH communs à Mérimée et OSM'''
-        salle_merosm=Musee('merosm')
-        for m,v in self.collection.items():
+            '''Créer une salle avec les MH communs à Mérimée, OSM et WP '''
+            if  v.description[m]['mer'] and v.description[m]['osm'] and v.description[m]['wip']:
+                list_salle[0].collection[m] = v.description[m]
+            ''' Créer une salle avec les MH communs à Mérimée et OSM'''
             if  v.description[m]['mer'] and v.description[m]['osm']:
-                salle_merosm.collection[m] = v.description[m]
-                v.cat="merosm"
-        #print(len(salle_merosm.collection))
-        return salle_merosm
+                list_salle[1].collection[m] = v.description[m]
+            ''' Créer une salle avec les MH communs à Mérimée et WP'''
+            if v.description[m]['mer'] and v.description[m]['wip']:
+                list_salle[2].collection[m] = v.description[m]
+            ''' Créer une salle avec les MH communs à OSM et WP '''
+            if  v.description[m]['osm'] and v.description[m]['wip']:
+                list_salle[3].collection[m] = v.description[m]
+            ''' Créer une salle avec les MH présents seulement dans Osm '''
+            if  not v.description[m]['mer'] and v.description[m]['osm'] and not v.description[m]['wip']:
+                list_salle[4].collection[m] = v.description[m]
+            ''' Créer une salle avec les MH présents seulement dans WP '''
+            if  not v.description[m]['mer'] and not v.description[m]['osm'] and v.description[m]['wip']:
+                list_salle[5].collection[m] = v.description[m]
+        return liste_salle
 
-    def chercher_Osm(self):
-        ''' Créer une salle avec les MH OSM qui ne sont pas dans Mérimée'''
-        salle_osm=Musee('osm')
-        for m,v in self.collection.items():
-            if not v.description[m]['mer'] and v.description[m]['osm'] and not "Bis" in m :
-                salle_osm.collection[m] = v.description[m]
-                v.cat="osm"
-        #print(len(salle_osm.collection))
-        return salle_osm
+    # def chercher_OsmWip(self,salle):
+    #     ''' Créer une salle avec les MH communs à OSM et WP '''
+    #     for m,v in self.collection.items():
+    #         if  v.description[m]['osm'] and v.description[m]['wip']:
+    #             salle.collection[m].description[m] = v.description[m]
+    #     return salle
+    #
+    # def chercher_Wip(self):
+    #     ''' Créer une salle avec les MH présents seulement dans WP '''
+    #     salle_wip=Musee('wip')
+    #     for m,v in self.collection.items():
+    #         if  not v.description[m]['mer'] and not v.description[m]['osm'] and v.description[m]['wip']:
+    #             salle_wip.collection[m] = v.description[m]
+    #     return salle_wip
+    #
+    # def chercher_MerOsm(self):
+    #     ''' Créer une salle avec les MH communs à Mérimée et OSM'''
+    #     salle_merosm=Musee('merosm')
+    #     for m,v in self.collection.items():
+    #         if  v.description[m]['mer'] and v.description[m]['osm']:
+    #             salle_merosm.collection[m] = v.description[m]
+    #     return salle_merosm
+    #
+    # def chercher_Osm(self):
+    #     ''' Créer une salle avec les MH OSM qui ne sont pas dans Mérimée'''
+    #     salle_osm=Musee('osm')
+    #     for m,v in self.collection.items():
+    #         if not v.description[m]['mer'] and v.description[m]['osm'] and not "Bis" in m :
+    #             salle_osm.collection[m] = v.description[m]
+    #     return salle_osm
 
 
 class MoHist:
@@ -65,14 +100,13 @@ class MoHist:
     '''
     ctr_monument=0
 
-    def __init__(self, ref_mhs=None):
+    def __init__(self, ref_mhs=None,cat=None):
         if ref_mhs:
             self.mhs=ref_mhs
         else:
             self.mhs = "no_mhs_"+str(MoHist.ctr_monument)
+
         self.description={self.mhs:{"mer":{},"osm":{},"wip":{}}}
-        #la catégorie du monument => résultat des tests en fonction de la source mer,osm,wip
-        self.cat=""
         MoHist.ctr_monument+=1
 
 def charge_merimee(dep,musee):
@@ -127,12 +161,19 @@ def charge_wp(d,musee):
     return musee
 
 if __name__ == "__main__":
-    salle_merosm=Musee('test')
-    salle_osm=Musee('test1')
-    StPierre=Musee()
+    base =['mer','osm','wip']
+    noms_salle= ['s_merosmwip','s_merosm','s_merwip','s_osmwip','s_osm','s_wip']
+    liste_salle=[]
+    for nom_salle in noms_salle :
+        nom_salle = Musee(nom_salle)
+        liste_salle.append(nom_salle)
+    #print(liste_salle[0])
+    # exit()
+    #par département
     for d in ini.dep:
         #print(d,ini.dep[d])
-
+        # Un musée par département !!
+        StPierre=Musee(ini.dep[d][d])
         StPierre = charge_merimee(d,StPierre)
         StPierre = charge_osm(ini.dep[d],StPierre)
         StPierre = charge_wp(ini.dep[d],StPierre)
@@ -149,14 +190,26 @@ if __name__ == "__main__":
         #         print(m)
             #print(value.description[m]['osm']['url_osm'])
         ''' qq stats'''
-    print("Nombre de MH dans le Musee {}".format(len(StPierre.collection)))
-        # print ("Nombre de MH issues de la base Mérimée {}".format(StPierre.calcul_nbMH_merimee()))
-        # print ("Nombre de MH issues de la base Osm {}".format(StPierre.calcul_nbMH_osm()))
+        print("Nombre de MH dans le Musee {}".format(len(StPierre.collection)))
+        for bs in base:
+            print ("Nombre de MH issues de la base {} : {}".format(bs,StPierre.calcul_nbMH(bs)))
+        # salle_wip = StPierre.chercher_Wip()
+        # print("Nombre de MH seulement dans WP {}".format(len(salle_wip.collection)))
+        # salle_osmwip = StPierre.chercher_OsmWip()
+        # print("Nombre de MH commun à OSM et WP {}".format(len(salle_osmwip.collection)))
+        liste_salle = StPierre.creer_list(liste_salle)
+        for n in range(len(liste_salle)):
+            print("Nombre de MH dans la salle : {} = {}".format(liste_salle[n].nom, len(liste_salle[n].collection)))
         # salle_merosm = StPierre.chercher_MerOsm()
-        # nb_salle_merosm = len(salle_merosm.collection)
-        # print("Nombre de MH commun à Mérimée et OSM {}".format(nb_salle_merosm))
+        # print("Nombre de MH commun à Mérimée et OSM {}".format(len(salle_merosm.collection)))
         # salle_osm = StPierre.chercher_Osm()
-        # nb_salle_osm = len(salle_osm.collection)
-        # print("Nombre de MH présents seulemnt dans OSM {}".format(nb_salle_osm))
+        # print("Nombre de MH présents seulemnt dans OSM {}".format(len(salle_osm.collection)))
         # print ()
-    print (MoHist.ctr_monument)
+        print (MoHist.ctr_monument)
+        # for mh in StPierre.collection:
+        #     #print(StPierre.collection[mh].description[mh])
+        #     if mh in salle_merosmwip.collection :
+        #         print(salle_merosmwip.collection[mh]['wip'])
+
+        # for mh in salle_merosmwip.collection:
+        #     print (mh,salle_merosmwip.collection[mh]['osm'] )
