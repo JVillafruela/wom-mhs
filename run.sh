d99cef98f3787088d5b124f6d5601392a2b88d25
JeaRRo <jean.ph.navarro@gmail.com>
2016-04-30 17:50:29 +0200

    Fin du script run.sh si la génération s'est planté

--- run.sh
+++ run.sh
@@ -29,21 +29,23 @@ cd ./Mhs
 
 #lancer l'éxécution de la génération
 python3 gen_html.py
+# Ne pas effectuer les synchros si le script s'est planté
+if [ $? -eq 0 ]; then
+    # gitter les pages web et les pousser sur le serveur web
 
-# gitter les pages web et les pousser sur le serveur web
+    cd ../Wom
+    git add -A
 
-cd ../Wom
-git add -A
+    message="Pages web générées le $jour à $heure"
+    #echo $message
+    #echo $base
+    git commit -am "$message"
+    git push
 
-message="Pages web générées le $jour à $heure"
-#echo $message
-#echo $base
-git commit -am "$message"
-git push
-
-################# à modifier pour la prod !!
-lftp ftp://user:password@ftp.server -e "mirror -e -R -x /var/services/homes/jean/web_wom/Wom/.git /var/services/homes/jean/web_wom/Wom/ /www/wom/ ; quit"
-#
-#
-# finir
- deactivate
+    ################# à modifier pour la prod !!
+    lftp ftp://user:password@ftp.server -e "mirror -e -R -x /var/services/homes/jean/web_wom/Wom/.git /var/services/homes/jean/web_wom/Wom/ /www/wom/ ; quit"
+    #
+    #
+    # finir
+     deactivate
+if
