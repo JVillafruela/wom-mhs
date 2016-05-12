6a00cf27ee876619706b736452c8a6b22dcce584
JeaRRo <jean.ph.navarro@gmail.com>
2016-05-12 23:34:49 +0200

    activation trois départements

--- ini.py
+++ ini.py
@@ -3,18 +3,18 @@
 
 # dictionnaire des départements à analyser
 dep = {
-        #    '01' : {
-        #         "name": ["Ain"],
-        #         "code": "01",
-        #         "text": "de l'Ain",
-        #         "url_d": ["/wiki/Liste_des_monuments_historiques_de_l'Ain"],
-        #             },
-            # '42' : {
-            #     "name": ["Loire"],
-            #     "code": "42",
-            #     "text": "de la Loire",
-            #     "url_d": ["/wiki/Liste_des_monuments_historiques_de_la_Loire"],
-            #     },
+           '01' : {
+                "name": ["Ain"],
+                "code": "01",
+                "text": "de l'Ain",
+                "url_d": ["/wiki/Liste_des_monuments_historiques_de_l'Ain"],
+                    },
+            '42' : {
+                "name": ["Loire"],
+                "code": "42",
+                "text": "de la Loire",
+                "url_d": ["/wiki/Liste_des_monuments_historiques_de_la_Loire"],
+                },
             '69': {
                 "name": ['Rhône','Métropole de Lyon'],
                 "code": "69",
@@ -30,7 +30,7 @@ dep = {
 
 # Attention ce répertoire doit être créer avant le lancement du programme !
 # racine des pages web
-prod=False
+prod=True
 # en local
 url_dev="/home/jean/osm/monuments_historiques"
 # sur Syno
