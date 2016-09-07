3ddb70c35b801ba446db8b8a830b7dc9e4d50c16
JeaRRo <jean.ph.navarro@gmail.com>
2016-09-07 13:10:53 +0200

    Ajout licence GPL

--- mohist.py
+++ mohist.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#
+#  Copyright 2016 JeaRRo <jean.navarro@laposte.net>
+#  http://wiki.openstreetmap.org/wiki/User:JeaRRo
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
 '''
     Organisation des infos MH une class Musee qui contient une collection de monuments (MoHist)
 '''
@@ -116,7 +134,7 @@ class Musee:
                     infos+="<li>"+tag_E+"</li>"
                 else:
                     #print ("Classement : {}".format(classement))+"</li>"
-                    infos+="<li>Classement : {}".format(classement)                
+                    infos+="<li>Classement : {}".format(classement)
                     tag_E="mhs:inscription_date="
                     infos+="<b>Import sans classement</b> </li>"
                 #print ("Source : Base Mérimée ouverte - avril 2016 ")
