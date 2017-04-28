9082de3a343c6a6b1f0f7756670e4cbfc2340c9e
JeaRRo <jean.ph.navarro@gmail.com>
2017-04-28 21:53:07 +0200

    Correction scrapping suite modif sur wikipédia

--- wikipedia.py
+++ wikipedia.py
@@ -195,7 +195,7 @@ def extrait_infos(datas):
     # print(geo)
 
     #  Code_Mhs - datas[4]
-    rep = datas[4].find('cite', {'style': 'font-style: normal'})
+    rep = datas[4].find('cite', {'style': 'font-style: normal;'})
     #  print(rep)
     if isinstance(rep, bs4.element.Tag):
         code_mhs = rep.get_text().strip()
